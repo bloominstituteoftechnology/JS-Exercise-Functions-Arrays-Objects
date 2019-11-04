d971ab8a17fd672c32845ffe033f462b76c0ddc4
Dan Levy <397632+justsml@users.noreply.github.com>
2019-11-03 17:24:55 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 // ⭐️ Example Challenge start ⭐️
 
 /**
- * ### Challenge `exampleFunction`
+ * ### Challenge `addNumbers`
  * 
  * @instructions
  * This function should be able to take two numbers as arguments
  * and return the result of adding them together.
  * 
- * For example, if we invoke `exampleFunction` passing 5 and 3,
+ * For example, if we invoke `addNumbers` passing 5 and 3,
  * the returned value should be 8.
+ *
+ * NOTE: This example has been completed for you.
 */
-function exampleFunction(num1, num2) {
+function addNumbers(num1, num2) {
   return num1 + num2;
 }
 
@@ -75,19 +77,65 @@ function makeSmartPerson(/* code here */) {
   /* code here */
 }
 
+
+
+
+
+/*
+// ⭐️ Example Test Data ⭐️
+
+var inventory = [
+  { id: 1, car_make: "Lincoln", car_model: "Navigator", car_year: 2009 },
+  { id: 2, car_make: "Mazda", car_model: "Miata MX-5", car_year: 2001 },
+  { id: 3, car_make: "Land Rover", car_model: "Defender Ice Edition", car_year: 2010 },
+  { id: 4, car_make: "Honda", car_model: "Accord", car_year: 1983 },
+  { id: 5, car_make: "Mitsubishi", car_model: "Galant", car_year: 1990 },
+  { id: 6, car_make: "Honda", car_model: "Accord", car_year: 1995 },
+  { id: 7, car_make: "Smart", car_model: "Fortwo", car_year: 2009 },
+  { id: 8, car_make: "Audi", car_model: "4000CS Quattro", car_year: 1987 },
+  { id: 9, car_make: "Ford", car_model: "Windstar", car_year: 1996 },
+  { id: 10, car_make: "Mercedes-Benz", car_model: "E-Class", car_year: 2000 },
+  { id: 11, car_make: "Infiniti", car_model: "G35", car_year: 2004 },
+  { id: 12, car_make: "Lotus", car_model: "Esprit", car_year: 2004 },
+  { id: 13, car_make: "Chevrolet", car_model: "Cavalier", car_year: 1997 },
+  { id: 14, car_make: "Dodge", car_model: "Ram Van 1500", car_year: 1999 }
+  /// ... Truncated
+]
+*/
+/**
+  * ### Example Array Challenge:
+  * 
+  * @instructions
+  * get3rdCar() should return the string `The car is a Land Rover Defender Ice Edition`
+  * 
+  *
+  * NOTE: This example has been completed for you.
+**/
+function get3rdCar(inventory) {
+  const the3rd = inventory.find((item, index) => {
+    return index === 2 // we use 2 because index is zero-based.
+  })
+  return `The car is a ${the3rd.car_make} ${the3rd.car_model}`
+}
+
+// 👇 COMPLETE YOUR WORK BELOW 👇
+// 👇 COMPLETE YOUR WORK BELOW 👇
+// 👇 COMPLETE YOUR WORK BELOW 👇
+
+
 /**
  * ### Challenge `getCarInfoByIndex`
  * 
  * @instructions
  * getCarInfoByIndex takes two arguments:
- *     (1) an array which is an inventory of cars like the one inside /data/inventory.js.
+ *     (1) an array which is an inventory of cars like the preview above (see ⭐️ Preview Test Data ⭐️)
  *     (2) a number which is the desired index in the array.
- * getCarInfoByIndex returns a string in the format `This is a {car_make} {car_model}
+ * getCarInfoByIndex returns a string in the format `This is a {car_make} {car_model}`
  * 
  * For example, if getCarInfoByIndex is invoked with the inventory and the number 0,
  * it will return `This is a Lincoln Navigator`.
 */
-function getCarInfoByIndex(/* code here */) {
+function getCarInfoByIndex(inventory, index) {
   /* code here */
 }
 
