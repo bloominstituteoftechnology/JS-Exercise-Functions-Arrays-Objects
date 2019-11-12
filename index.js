add0aa2855a1282e444f611908148cc7740cd907
“C <carolinehstinger@gmail.com>
2019-11-12 16:17:08 -0500

    adds third solution

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ makePersonObject(5, "Leia","leia@leia.com");
 function getName({name}) {
   return `Hello, my name is ${name}`
 };
-getName ({ id: 1, name: 'Leia', email: 'leia@leia.com' });
+getName ({ id:1, name: 'Leia', email: 'leia@leia.com' });
 
 /**
  * ### Challenge `makeSmartPerson`
@@ -80,13 +80,18 @@ getName ({ id: 1, name: 'Leia', email: 'leia@leia.com' });
  *         and returns a string like `Hello, my name is {name}`.
  *         where `{name}` is the name passed into `makeSmartPerson`.
 */
-function makeSmartPerson(/* code here */) {
-  /* code here */
-}
-
-
-
-
+function makeSmartPerson({name}) {
+  const smartPerson = {
+    name: name,
+    sum: function(num1, num2) {
+      return num1 + num2;
+    },
+    speak: function() {
+      return `Hello, my name is {name}`;
+    }
+  }
+  return smartPerson;
+};
 
 /*
 // ⭐️ Example Test Data ⭐️
