344224339c8f5025e34adca8a7fc90e2b32bc287
Amber Robinson <ballewaer@gmail.com>
2019-12-05 17:58:03 -0800

    streach

--- index.js
+++ index.js
@@ -309,8 +309,16 @@ const argTimesTwo = num => num * 2; // code here!
  *         (1) causes the odometer in the object to be increased by the distance,
  *         (2) returns the updated value of the `odometer`.
 */
-function carMaker(/* code here */) {
+function carMaker(MaterTruck) {
   /* code here */
+  const car = {
+    odometer: MaterTruck, 
+    drive: function(distance) {
+      this.odometer += distance;
+      return this.odometer;
+    } 
+  };
+  return car;
 }
 
 /// ////// END OF CHALLENGE /////////
