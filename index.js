16477b272598448a6700e643e05e0e0377428f71
Jason <Jason@MacBook-Pro-3.local>
2019-12-06 01:24:09 -0800

    getLastCarInfo completed

--- index.js
+++ index.js
@@ -173,8 +173,9 @@ function getCarInfoByIndex(inventory, index) {
  * For example, if getLastCarInfo is invoked passing the inventory inside /data/inventory.js,
  * it will return `This is a Lincoln Town Car`.
 */
-function getLastCarInfo(/* code here */) {
-  /* code here */
+function getLastCarInfo(invArray) {
+  const lastCar =  invArray[invArray.length - 1];
+  return `This is a ${lastCar.car_make} ${lastCar.car_model}`;
 }
 
 /**
