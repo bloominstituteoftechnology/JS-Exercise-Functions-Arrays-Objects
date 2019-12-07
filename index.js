15624fd1688971a3b1ed99c5a791b54dbbba886e
Spencer_Vail <vail.spencer@gmail.com>
2019-12-06 17:21:45 -0700

    stretch

--- index.js
+++ index.js
@@ -284,8 +284,15 @@ const argTimesTwo = (num) => {return num * 2}; // code here!
  *         (1) causes the odometer in the object to be increased by the distance,
  *         (2) returns the updated value of the `odometer`.
 */
-function carMaker(/* code here */) {
-  /* code here */
+function carMaker(odometer) {
+  let carTrip = {
+    odometer:odometer,
+    drive: function (distance) {
+     this.odometer = this.odometer + distance;
+     return this.odometer; 
+    }
+  }
+  return carTrip;
 }
 
 /// ////// END OF CHALLENGE /////////
