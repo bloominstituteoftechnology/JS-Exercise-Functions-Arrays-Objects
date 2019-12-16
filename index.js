fb868ad5ebf4075f571ed168c60cdf80adcce8b5
mastaryan <58410246+mastaryan@users.noreply.github.com>
2019-12-16 15:57:12 -0500

    getName Challenge

--- index.js
+++ index.js
@@ -61,8 +61,8 @@ function makePersonObject(id, name, email) {
  * passing { id: 1, name: 'Leia', email: 'leia@leia.com` } as the argument,
  * the returned value should look like `Hello, my name is Leia`.
 */
-function getName(/* code here */) {
-  /* code here */
+function getName(objectName) {
+  return 'Hello, my name is' + objectName.name;
 }
 
 /**
