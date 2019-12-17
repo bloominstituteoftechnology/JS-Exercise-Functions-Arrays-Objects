d27e954b152fc3cc3d37a3796a2bad41dd507f08
mastaryan <58410246+mastaryan@users.noreply.github.com>
2019-12-16 22:13:36 -0500

    Challenge 1

--- index.js
+++ index.js
@@ -298,9 +298,9 @@ function getGermanCars(inventory) {
  *   return num * 2
  * }
 */
-const sum = null; // code here!
-const addFive = null; // code here!
-const argTimesTwo = null; // code here!
+const sum = (a, b) => a+b
+const addFive = (num) => num +5;
+const argTimesTwo = (num) => num*2;
 
 /**
  * ### Challenge `carMaker`
