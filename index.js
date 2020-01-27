944da2cf152d2598959b5dddbffdd045540cc9d2
Alexander Karren <alexanderkarren@Alexanders-MacBook-Pro.local>
2020-01-27 14:56:06 -0700

    refactoring passed

--- index.js
+++ index.js
@@ -295,9 +295,15 @@ function getGermanCars(inventory) {
  *   return num * 2
  * }
 */
-const sum = null; // code here!
-const addFive = null; // code here!
-const argTimesTwo = null; // code here!
+const sum = (a, b) => {
+  return a +b;
+};
+const addFive = (num) => {
+  return num + 5;
+};
+const argTimesTwo = (num) => {
+  return num * 2;
+};
 
 /**
  * ### Challenge `carMaker`
