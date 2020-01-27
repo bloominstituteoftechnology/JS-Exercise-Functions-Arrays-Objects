18a0e17c7234068bc901fcd5af1a6f38f9d2edd5
Alexander Karren <alexanderkarren@Alexanders-MacBook-Pro.local>
2020-01-27 14:50:51 -0700

    passed getOlderCars()

--- index.js
+++ index.js
@@ -246,8 +246,14 @@ function getModelYears(inventory) {
  * with a `car_year` which is at most the given desired max year,
  * in the same order as they appear in the original inventory.
 */
-function getOlderCars(/* code here */) {
-  /* code here */
+function getOlderCars(inventory, maxYear) {
+  const olderCars = [];
+  for (let i = 0; i < inventory.length; i++) {
+    if (inventory[i].car_year <= maxYear) {
+      olderCars.push(inventory[i]);
+    }
+  }
+  return olderCars;
 }
 
 /**
