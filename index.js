c07890707c23a69f4f197e7d09e4a0d244f5ec01
Alexander Karren <alexanderkarren@Alexanders-MacBook-Pro.local>
2020-01-27 14:53:56 -0700

    passed getGermanCars()

--- index.js
+++ index.js
@@ -267,8 +267,14 @@ function getOlderCars(inventory, maxYear) {
  * made by either `Audi` or `Mercedes-Benz` or `Volkswagen` or `BMW`,
  * in the same order as they appear in the original inventory.
 */
-function getGermanCars(/* code here */) {
-  /* code here */
+function getGermanCars(inventory) {
+  const germanCars = [];
+  for (let i =0; i < inventory.length; i++) {
+    if (inventory[i].car_make === "Audi" || inventory[i].car_make === "Mercedes-Benz" || inventory[i].car_make === "Volkswagen" || inventory[i].car_make === "BMW") {
+      germanCars.push(inventory[i]);
+    }
+  }
+  return germanCars;
 }
 
 /**
