ae5f9b04ed17540ab3ba382e9b34a763c630ea7d
sam-h-hall <sam_h_hall@icloud.com>
2020-02-11 06:45:27 -0600

    first 2

--- index.js
+++ index.js
@@ -39,9 +39,15 @@ function addNumbers(num1, num2) {
  *   email: "leia@leia.com",
  * }
 */
-function makePersonObject(/* code here */) {
-  /* code here */
+function makePersonObject(idNumber, name, email) {
+  const object = {
+    id: idNumber,
+    name: name,
+    email: email
+  }
+  return object;
 }
+
 
 /**
  * ### Challenge `getName`
@@ -56,8 +62,10 @@ function makePersonObject(/* code here */) {
  * passing { id: 1, name: 'Leia', email: 'leia@leia.com` } as the argument,
  * the returned value should look like `Hello, my name is Leia`.
 */
-function getName(/* code here */) {
-  /* code here */
+function getName(object) {
+  let name = object.name;
+
+  return `Hello, my name is ${name}`;
 }
 
 /**
