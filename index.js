0e5db4742b79d5d2cc871fb1562fbc1c941eec2b
Mark Rivera <Mriver1991@gmail.com>
2020-02-12 16:14:20 -0500

    finished stretch goals

--- index.js
+++ index.js
@@ -323,8 +323,18 @@ const argTimesTwo = num => num * 2; // code here!
  *         (1) causes the odometer in the object to be increased by the distance,
  *         (2) returns the updated value of the `odometer`.
 */
-function carMaker(/* code here */) {
-  /* code here */
+function carMaker(odometer) {
+  let obj = {
+    odometer
+  }
+
+  function addDistance (distance) {
+    this.odometer = this.odometer + distance;
+    return this.odometer
+  }
+
+  obj.drive = addDistance;
+  return obj
 }
 
 /// ////// END OF CHALLENGE /////////
