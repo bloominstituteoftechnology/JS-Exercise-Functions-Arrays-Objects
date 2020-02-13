f4378feb4670396571a271167871fa077dd7ce5c
Lambda_School_Loaner_282 <lambda_school_loaner_282@Lambda-School-Loaner-282s-MacBook-Pro.local>
2020-02-12 22:37:09 -0600

    Sixth push.  I'm getting somewhere

--- index.js
+++ index.js
@@ -64,9 +64,18 @@ console.log(makePersonObject);
  * passing { id: 1, name: 'Leia', email: 'leia@leia.com` } as the argument,
  * the returned value should look like `Hello, my name is Leia`.
 */
-function getName(/* code here */) {
+function getName(name) {
   /* code here */
+  let Obj = {nameObj: "Hello, my name is " +name}
+  return (Obj['nameObj']);
 }
+
+const person = {id: 7,
+  studentName: "Theresa M Lennon",
+  email:"oddyseyt@gmail.com"
+}
+
+console.log(getName(person.studentName));
 
 /**
  * ### Challenge `makeSmartPerson`
@@ -81,10 +90,15 @@ function getName(/* code here */) {
  *         and returns a string like `Hello, my name is {name}`.
  *         where `{name}` is the name passed into `makeSmartPerson`.
 */
-function makeSmartPerson(/* code here */) {
+function makeSmartPerson(name) {
   /* code here */
-}
-
+    return {
+      name: name,
+      sum: function(num1, num2) { return num1 + num2; },
+      speak: function () { return `Hello, my name is ${name}`; }
+    };
+  }
+  
 
 
 
@@ -145,7 +159,29 @@ function get3rdCar(inventory) {
 */
 function getCarInfoByIndex(inventory, index) {
   /* code here */
+  const carID = inventory[index]
+  
+  return `This is a ${carID.car_make} ${carID.car_model}`
 }
+
+const carStuff = [
+  { id: 1, car_make: "Lincoln", car_model: "Navigator", car_year: 2009 },
+  { id: 2, car_make: "Mazda", car_model: "Miata MX-5", car_year: 2001 },
+  { id: 3, car_make: "Land Rover", car_model: "Defender Ice Edition", car_year: 2010 },
+  { id: 4, car_make: "Honda", car_model: "Accord", car_year: 1983 },
+  { id: 5, car_make: "Mitsubishi", car_model: "Galant", car_year: 1990 },
+  { id: 6, car_make: "Honda", car_model: "Accord", car_year: 1995 },
+  { id: 7, car_make: "Smart", car_model: "Fortwo", car_year: 2009 },
+  { id: 8, car_make: "Audi", car_model: "4000CS Quattro", car_year: 1987 },
+  { id: 9, car_make: "Ford", car_model: "Windstar", car_year: 1996 },
+  { id: 10, car_make: "Mercedes-Benz", car_model: "E-Class", car_year: 2000 },
+  { id: 11, car_make: "Infiniti", car_model: "G35", car_year: 2004 },
+  { id: 12, car_make: "Lotus", car_model: "Esprit", car_year: 2004 },
+  { id: 13, car_make: "Chevrolet", car_model: "Cavalier", car_year: 1997 },
+  { id: 14, car_make: "Dodge", car_model: "Ram Van 1500", car_year: 1999 }
+  /// ... Truncated
+]
+console.log(getCarInfoByIndex(carStuff, 3));
 
 /**
  * ### Challenge `getLastCarInfo`
