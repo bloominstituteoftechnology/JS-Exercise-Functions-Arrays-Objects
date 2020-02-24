ff0c433549c136b31716420e3474c2c42e3d254f
evan <aspaasevan@gmail.com>
2020-02-24 10:54:48 -0800

    object challenges complete and passing

--- index.js
+++ index.js
@@ -39,8 +39,14 @@ function addNumbers(num1, num2) {
  *   email: "leia@leia.com",
  * }
 */
-function makePersonObject(/* code here */) {
-  /* code here */
+function makePersonObject(id, name, email) {
+  const me = {
+    id: id,
+    name: name, 
+    email: email
+  };
+  
+  return me;
 }
 
 /**
@@ -56,8 +62,8 @@ function makePersonObject(/* code here */) {
  * passing { id: 1, name: 'Leia', email: 'leia@leia.com` } as the argument,
  * the returned value should look like `Hello, my name is Leia`.
 */
-function getName(/* code here */) {
-  /* code here */
+function getName(nameObj) {
+  return "Hello, my name is " + nameObj.name;
 }
 
 /**
@@ -73,8 +79,17 @@ function getName(/* code here */) {
  *         and returns a string like `Hello, my name is {name}`.
  *         where `{name}` is the name passed into `makeSmartPerson`.
 */
-function makeSmartPerson(/* code here */) {
-  /* code here */
+function makeSmartPerson(name) {
+  const myObj = {
+    name: name, 
+    sum: function(num1, num2) {
+      return num1 + num2;
+    },
+    speak: function() {
+      return "Hello, my name is " + name;
+    }
+  };
+  return myObj;
 }
 
 
