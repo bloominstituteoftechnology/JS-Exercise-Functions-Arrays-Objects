485b822dd51c5be8e3446b98070cb6f54caaf064
Lambda_School_Loaner_273 <lambda_school_loaner_273@Lambda-School-Loaner-273s-MacBook-Pro.local>
2020-02-25 15:07:30 -0500

    js2

--- index.js
+++ index.js
@@ -39,9 +39,11 @@ function addNumbers(num1, num2) {
  *   email: "leia@leia.com",
  * }
 */
-function makePersonObject(/* code here */) {
-  /* code here */
+function makePersonObject(id, name, email){
+  return {id, name, email};
 }
+console.log(makePersonObject(5, "Leia", "leia@leia.com",));
+
 
 /**
  * ### Challenge `getName`
@@ -56,8 +58,8 @@ function makePersonObject(/* code here */) {
  * passing { id: 1, name: 'Leia', email: 'leia@leia.com` } as the argument,
  * the returned value should look like `Hello, my name is Leia`.
 */
-function getName(/* code here */) {
-  /* code here */
+function getName(object) {
+     return "Hello, my name is " + object.name
 }
 
 /**
@@ -73,8 +75,8 @@ function getName(/* code here */) {
  *         and returns a string like `Hello, my name is {name}`.
  *         where `{name}` is the name passed into `makeSmartPerson`.
 */
-function makeSmartPerson(/* code here */) {
-  /* code here */
+function makeSmartPerson(object) {
+  
 }
 
 
@@ -136,7 +138,7 @@ function get3rdCar(inventory) {
  * it will return `This is a Lincoln Navigator`.
 */
 function getCarInfoByIndex(inventory, index) {
-  /* code here */
+    return 'This is a ${inventory[index].car_make} ${inventory[index].car_model}';
 }
 
 /**
@@ -150,8 +152,8 @@ function getCarInfoByIndex(inventory, index) {
  * For example, if getLastCarInfo is invoked passing the inventory inside /data/inventory.js,
  * it will return `This is a Lincoln Town Car`.
 */
-function getLastCarInfo(/* code here */) {
-  /* code here */
+function getLastCarInfo(inventory) {
+   return 'This is a inventory ${inventory[inventory.length-1].car_make} ${inventory[inventory.length-1].car_model}';
 }
 
 /**
