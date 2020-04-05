a629f78f25d3577f895965b6d10cff4ed1dd5a8e
Sawyer Pearson <sawyer.pearson92@gmail.com>
2020-04-05 11:11:23 -0600

    getName function completed

--- index.js
+++ index.js
@@ -99,8 +99,9 @@ function temperatureInF(degrees, unit) {
  *   email: "leia@leia.com",
  * }
 */
-function makePersonObject(/* code here */) {
-  /* code here */
+function makePersonObject(id, name, email) {
+  Object.create(person);
+  return person;
 }
 
 /**
@@ -116,8 +117,8 @@ function makePersonObject(/* code here */) {
  * passing { id: 1, name: 'Leia', email: 'leia@leia.com` } as the argument,
  * the returned value should look like `Hello, my name is Leia`.
 */
-function getName(/* code here */) {
-  /* code here */
+function getName(person) {
+  return 'Hello, my name is ' + person.name + '.' 
 }
 
 
