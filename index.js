f6d934c61b49d1a2f365e3ac5a11b57ad483d5f6
Allison Usher <allyjay317@gmail.com>
2020-04-21 21:17:50 -0700

    completed getModelYears

--- index.js
+++ index.js
@@ -253,8 +253,13 @@ function getLastCarInfo(inventory) {
  *     (1) an array which is an inventory of cars like the one inside /data/inventory.js.
  * getModelYears returns an array containing all the 'car_year's in the inventory.
 */
-function getModelYears(/* code here */) {
-  /* code here */
+function getModelYears(inventory) {
+  let years = [];
+  inventory.forEach(element => {
+    years.push(element.car_year);
+    
+  });
+  return years;
 }
 
 /**
