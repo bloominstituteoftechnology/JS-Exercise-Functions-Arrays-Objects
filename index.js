072b27c9215d6e139a1e3e1d3ecea20cdc570ca9
dustin <dustnclaytiner@gmail.com>
2020-04-21 22:54:56 -0500

    another one down

--- index.js
+++ index.js
@@ -123,10 +123,11 @@ makePersonObject(5, "Leia", "Leia@leia.com");
  * passing { id: 1, name: 'Leia', email: 'leia@leia.com` } as the argument,
  * the returned value should look like `Hello, my name is Leia`.
 */
-function getName(/* code here */) {
-  /* code here */
-}
+function getName(para1) {
 
+  return "Hello, my name is " + para1.name;
+}
+getName ( {id: 1, name: 'Leia', email: 'leia@leia.com' });
 
 /**
  * ### Challenge `appleIndex`
