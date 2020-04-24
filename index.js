a92dfccf993530ddf9874b42ef105f992d4bf58b
Robel Mengistu <62121236+robelv2020@users.noreply.github.com>
2020-04-23 18:46:19 -0600

    get car was made using two ways ,top one was long

--- index.js
+++ index.js
@@ -355,8 +355,36 @@ function getOlderCars(inventory ,maxYear) {
  * made by either `Audi` or `Mercedes-Benz` or `Volkswagen` or `BMW`,
  * in the same order as they appear in the original inventory.
 */
-function getGermanCars(/* code here */) {
-  /* code here */
+/*
+// ⭐️ Example Test Data ⭐️
+
+var inventory = [
+  { id: 1, car_make: "Lincoln", car_model: "Navigator", car_year: 2009 },
+  { id: 2, car_make: "Mazda", car_model: "Miata MX-5", car_year: 2001 },
+  { id: 3, car_make: "Land Rover", car_model: "Defender Ice Edition", car_year: 2010 },
+  { id: 4, car_make: "Honda", car_model: "Accord", car_year: 1983 },
+  { id: 5, car_make: "Mitsubishi", car_model: "Galant", car_year: 1990 },
+  { id: 6, car_make: "Honda", car_model: "Accord", car_year: 1995 },
+  { id: 7, car_make: "Smart", car_model: "Fortwo", car_year: 2009 },
+  { id: 8, car_make: "Audi", car_model: "4000CS Quattro", car_year: 1987 },
+  { id: 9, car_make: "Ford", car_model: "Windstar", car_year: 1996 },
+  { id: 10, car_make: "Mercedes-Benz", car_model: "E-Class", car_year: 2000 },
+  { id: 11, car_make: "Infiniti", car_model: "G35", car_year: 2004 },
+  { id: 12, car_make: "Lotus", car_model: "Esprit", car_year: 2004 },
+  { id: 13, car_make: "Chevrolet", car_model: "Cavalier", car_year: 1997 },
+  { id: 14, car_make: "Dodge", car_model: "Ram Van 1500", car_year: 1999 }
+  /// ... Truncated
+]
+*/
+
+function getGermanCars(inventory) {
+  let gerCar = [];
+  for(let i=0; i<= inventory.length-1;i++){
+    if(inventory[i].car_make === 'Audi' || inventory[i].car_make === 'Mercedes-Benz' || inventory[i].car_make === 'Volkswagen' || inventory[i].car_make === 'BMW' ){
+      gerCar.push(inventory[i]);
+    }
+  }
+  return gerCar;
 }
 
 /**
@@ -372,8 +400,21 @@ function getGermanCars(/* code here */) {
  *         (1) causes the odometer in the object to be increased by the distance,
  *         (2) returns the updated value of the `odometer`.
 */
-function carMaker(/* code here */) {
-  /* code here */
+function carMaker(odometerNum) {
+  // let x , y;
+  // y = {
+  //   odometer: odometerNum, drive:function(distance){
+  //     x = (this.odometer+=distance);
+  //     return x;
+  //   }
+  // };
+  // return y;
+    return{
+      odometer:odometerNum , drive: function(distance){
+        return (this.odometer += distance);
+      }
+    };
+
 }
 
 /// ////// END OF CHALLENGE /////////
