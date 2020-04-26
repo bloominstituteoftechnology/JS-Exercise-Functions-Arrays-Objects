1cbcdc52c8e29a40155d5be2dcbd7976da5601f3
Mitchell Wright <mitchellbwright@gmail.com>
2020-04-26 11:58:40 -0600

    pass test getModelYears

--- index.js
+++ index.js
@@ -257,8 +257,14 @@ function getLastCarInfo(inventory) {
  *     (1) an array which is an inventory of cars like the one inside /data/inventory.js.
  * getModelYears returns an array containing all the 'car_year's in the inventory.
 */
-function getModelYears(/* code here */) {
-  /* code here */
+function getModelYears(inventory) {
+  let carYears = [];
+
+  for(let i=0; i<inventory.length; i++) {
+    carYears.push(inventory[i].car_year);
+  }
+
+  return carYears;
 }
 
 /**
