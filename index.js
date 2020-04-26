fa46b505d6a69378a2e7d27ce028c3a6616708e7
Mitchell Wright <mitchellbwright@gmail.com>
2020-04-26 16:05:36 -0600

    pass test getCarInfoById

--- index.js
+++ index.js
@@ -281,8 +281,12 @@ function getModelYears(inventory) {
  * For example, if getCarInfoById is invoked with the inventory and the number 1,
  * it will return `This is a Lincoln Navigator`.
 */
-function getCarInfoById(/* code here */) {
-  /* code here */
+function getCarInfoById(inventory, id) {
+  for(let i=0; i<inventory.length; i++) {
+    if(inventory[i].id === id) {
+      return `This is a ${inventory[i].car_make} ${inventory[i].car_model}`;
+    }
+  }
 }
 
 /**
