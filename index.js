fa376ba5abadc90d1e51237da975a6330d498477
JenVest2020 <jenvest2019@gmail.com>
2020-04-28 23:43:31 -0500

    missed the Last MVP, so I did it tonight

--- index.js
+++ index.js
@@ -247,8 +247,12 @@ function getLastCarInfo(Array) {
  *     (1) an array which is an inventory of cars like the one inside /data/inventory.js.
  * getModelYears returns an array containing all the 'car_year's in the inventory.
  */
-function getModelYears(/* code here */) {
-  /* code here */
+function getModelYears(carArray) {
+  let modelYears = [];
+  for (let i = 0; i < carArray.length; i++) {
+    modelYears.push(carArray[i].car_year);
+  }
+  return modelYears;
 }
 
 /**
