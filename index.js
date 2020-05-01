d108c1d1d870e040a0825738b9596cd9d3c65776
unknown <clay2000lbs@gmail.com>
2020-04-30 21:38:08 -0500

    Task 1 done.

--- index.js
+++ index.js
@@ -35,8 +35,8 @@ function addNumbers(num1, num2) {
  * the returned value should look like: 'Goodbye, Andy. Have a great day.'
  * 
 */
-function sayGoodbye(/* code here */) {
-  /* code here */
+function sayGoodbye(name) {
+  return `Goodbye, ${name}. Have a great day.`
 }
 
 /**
